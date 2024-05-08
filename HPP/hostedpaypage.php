205908a3382185faf746fc2cba1288d43897752a
gpreston <glenn.preston@evopayments.com>
2024-05-08 12:51:06 +0100

    Updated style of HostedPayPage and added option for card on file params

--- HPP/hostedpaypage.php
+++ HPP/hostedpaypage.php
@@ -25,8 +25,12 @@
 
 			<environmentselector-component></environmentselector-component>
 
+			<hr style="height:2px; border-width:0; color:gray; background-color:lightgray" />
+			<br />
+
 			<h2 class="mb-4">Step 1: Session Token Request</h2>
 			<form id="tokenForm">
+				<div class="text text-center mb-3"><h6><strong>Required Parameters</strong></h6></div>
 				<div class="row">
 					<div class="col">
 						<div class="form-group">
@@ -94,6 +98,56 @@
 						</div>
 					</div>
 				</div>
+
+				<div class="row justify-content-center mb-4">
+					<div class="form-check-inline">
+						<h6 class="mb-0"><strong>Card On File Parameters:</strong></h6>
+					</div>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="radio" class="form-check-input" name="cofOption" onclick="COFParams(this.value)" value="Not Included" checked="checked">Not Included
+						</label>
+					</div>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="radio" class="form-check-input" name="cofOption" onclick="COFParams(this.value)" value="Initial">Initial
+						</label>
+					</div>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="radio" class="form-check-input" name="cofOption" onclick="COFParams(this.value)" value="Subsequent" disabled>Subsequent
+						</label>
+					</div>
+				</div>
+
+				<div class="row cofParam">
+					<div class="col">
+						<div class="form-group">
+							<label class="m-0" for="cardOnFileTypeInput">cardOnFileType</label>
+							<select class="form-control form-control-sm cofParam" id="cardOnFileTypeInput" name="cardOnFileType">
+								<option  value="First">First</option>
+								<option  value="Repeat">Repeat</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="col">
+					<div class="form-group">
+							<label class="m-0" for="cardOnFileReasonInput">cardOnFileReason</label>
+							<select class="form-control form-control-sm cofParam" id="cardOnFileReasonInput" name="cardOnFileReason">
+								<option  value="C">C</option>
+								<option  value="I">I</option>
+								<option  value="R">R</option>
+								<option  value="H">H</option>
+								<option  value="E">E</option>
+								<option  value="D">D</option>
+								<option  value="M">M</option>
+								<option  value="N">N</option>
+							</select>
+						</div>
+					</div>
+				</div>
+
 				<div class="row justify-content-center">
 					<button class="btn btn-primary btn-lg my-4" type="submit">Submit</button>
 				</div>
@@ -105,9 +159,10 @@
 			</div>
 
 			<br />
+			<hr style="height:2px; border-width:0; color:gray; background-color:lightgray" />
+			<br />
 
 			<h2 class="my-4">Step 2: Action Request</h2>
-
 			<form id="actionForm">
 				<div class="row">
 					<div class="col">
@@ -139,12 +194,42 @@
 			function initSession() {
 				setTimestamp();
 				getUserConfigParams();
+				COFParams("Not Included");
+			}
+
+			function COFParams(value) {
+				console.log(value);
+				if (value == "Not Included") {
+					document.querySelectorAll(".cofParam").forEach(element => {
+						element.disabled = true;
+						element.style.display = "none";
+					});
+				}
+				else {
+					document.querySelectorAll(".cofParam").forEach(element => {
+						element.disabled = false;
+						element.style.display = "flex";
+					});
+				}
 			}
 			
 			$("#tokenForm").on("submit",function(e) {
 				e.preventDefault();
 				submitTokenForm(this);
 			});
+
+			function ToggleFieldDisabledStatus(fieldName, diasbledStatus) {
+				document.getElementsByName(fieldName).forEach((e) => {
+					e.disabled = diasbledStatus;
+				});
+			}
+
+			var cofNotIncluded = {};
+			var cofInitial = {
+				"cardOnFileType": "First",
+				"cardOnFileInitiator": "Cardholder",
+				"cardOnFileReason": "C"
+			};
 		</script>
 		
 		<!-- JS functions -->
